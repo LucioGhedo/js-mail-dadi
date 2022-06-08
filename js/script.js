b90fac6a4c393fa8d343f0c5a7437b63899aa2e4
LucioGhedo <lucioghedina@gmail.com>
2022-06-08 15:52:37 +0200

    add num output

--- js/script.js
+++ js/script.js
@@ -30,7 +30,9 @@ if(mailFound === true) {
 // estrazione numeri
 let randomPcNumber = Math.floor(Math.random() * 6) + 1;
 let randomUserNumber = Math.floor(Math.random() * 6) + 1;
-
+let numOutput =`Il numero del computer è: ${randomPcNumber}. Il tuo numero è: ${randomUserNumber}`;
+let deskNum = document.getElementById('numeri')
+deskNum.innerHTML = numOutput
 // comparo chi vince e stampo
 if(randomPcNumber > randomUserNumber) {
     diceResult.innerHTML = `${randomPcNumber}>${randomUserNumber} HA VINTO IL COMPUTER`
